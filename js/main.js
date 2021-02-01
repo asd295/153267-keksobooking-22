33d6a9920c1ecc144d0a25f4f334e1bd5979b297
zolotuhina julia <asd295@yandex.ru>
2021-02-01 13:54:25 +0500

    Утилитарные функции #1правки

--- js/main.js
+++ js/main.js
@@ -7,11 +7,12 @@ https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Ma
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-};
-
+}
+getRandomInteger (3,10);
 //возвращающает случайное число с плавающей точкой из переданного диапазона включительно.
 function getRandomFloat(min, max, numberPlaces) {
-  return (Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-
+  
+  const result = Math.random() * (max - min) + min;
   return result.toFixed(numberPlaces);
-};
+}
+getRandomFloat (3.444,4,777)
